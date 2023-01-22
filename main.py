1ab8dc03a8ea03f22d849d407a48c439d514d478
Artin GH <artinghazizadeh86@gmail.com>
2023-01-22 17:23:06 +0330

    Optimize the code

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import ElementClickInterceptedException
 from time import sleep
 from pynput.keyboard import Controller as KBController
 
+
+def remove_cookie_modal():
+    driver.execute_script('return document.querySelector("#qc-cmp2-main").remove()')
+
+
 kbc = KBController()
 
-chrome = Chrome()
-chrome.get('https://www.typing.com/student/login')
-chrome.find_element(By.CSS_SELECTOR, '#form-ele-username').send_keys('<username>')
-chrome.execute_script('document.querySelector("#form-ele-password").value = "<password>"')
-nxt = chrome.find_element(By.XPATH, '//*[@id="app"]/div/div/main/div/div[4]/div[2]/div[2]/div[2]/button')
-try:
-    nxt.click()
-except ElementClickInterceptedException:
-    chrome.find_element(By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]').click()
-    nxt.click()
-sleep(3)
-nxt.click()
-del nxt
-sleep(2)
-chrome.get('https://www.typing.com/student/typing-test/1-minute')
+driver = Chrome()
+driver.get('https://www.typing.com/student/typing-test/1-minute')
+remove_cookie_modal()
+driver.find_element(By.TAG_NAME, 'body').send_keys('\n')
+sleep(0.3)
+remove_cookie_modal()
+
+text_input = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[1]/div/div/div[2]/div[2]/div[1]/div/div/div')
 typing = True
 while typing:
-    txt = chrome.find_element(By.XPATH, '//*[@id="app"]/div/div[1]/div/div/div[2]/div[2]/div[1]/div/div/div').text
-    txt = txt.replace('\n', '').replace('  ', ' ')
-    for c in txt:
+    text = text_input.text
+    text = text.replace('\n', '').replace('  ', ' ')
+    for c in text:
         kbc.tap(c)
         sleep(0.1)
     sleep(0.1)
+
+driver.quit()
